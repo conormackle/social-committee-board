9c41486221cef0a585d6889a3aafdcb9aea4e2dd
hbrankinAQ <harry.brankin@aquaq.com>
2022-02-01 15:42:29 +0000

    Added createUser endpoint and securityconfig to allow post requests

--- scb/src/main/java/com/aquaq/scb/entities/users/UsersService.java
+++ scb/src/main/java/com/aquaq/scb/entities/users/UsersService.java
@@ -2,10 +2,15 @@ package com.aquaq.scb.entities.users;
 
 import com.aquaq.scb.response.ScbResponse;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.beans.FeatureDescriptor;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 @Log4j2
 @Service
@@ -38,5 +43,28 @@ public class UsersService {
         }catch(Exception e){
             return ScbResponse.createExceptionResponse(e);
         }
+    }
+
+    public ScbResponse updateUser(UsersModel usersModel, Integer userId){
+        try {
+            UsersModel updatedUsersModel = new UsersModel();
+            Optional<UsersModel> savedUsersModel = usersRepository.findById(userId);
+            UsersModel updateUser = savedUsersModel.get();
+            if(savedUsersModel.isPresent()){
+                BeanUtils.copyProperties(usersModel, updateUser, getNullPropertyNames(usersModel));
+            }
+            updatedUsersModel = usersRepository.save(updateUser);
+            return ScbResponse.createSuccessResponse(updatedUsersModel);
+        }catch(Exception e){
+            return ScbResponse.createExceptionResponse(e);
+        }
+    }
+
+    public static String[] getNullPropertyNames(Object source) {
+        final BeanWrapper wrappedSource = new BeanWrapperImpl(source);
+        return Stream.of(wrappedSource.getPropertyDescriptors())
+                .map(FeatureDescriptor::getName)
+                .filter(propertyName -> wrappedSource.getPropertyValue(propertyName) == null)
+                .toArray(String[]::new);
     }
 }
