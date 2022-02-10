2f7a9c22f7b43c5f360449ffeb5440993d188e2f
Dominic Reynolds <dominic.reynolds@aquaq.co.uk>
2022-02-10 17:53:44 +0000

    employee-map

---
+++ front-end/src/Content/EmployeeMap/index.js
@@ -0,0 +1,73 @@
+import ReactMapGL, { Marker } from 'react-map-gl'
+import { useState } from 'react'
+import "mapbox-gl/dist/mapbox-gl.css";
+import './index.scss'
+
+const mapboxAccessToken = 'pk.eyJ1IjoiZHJleW5vbGRzYXF1YXEiLCJhIjoiY2t6aDZiaTU0MDE5MDJvcDM1eG83aDAxZCJ9.nbsoY5m7_XSWqj78RwdAJA'
+
+const mapStyle = {
+  width: '100%',
+  height: '100%'
+}
+
+export default function EmployeeMap() {
+  const [markers, setMarkers] = useState([
+    {
+      name: 'Belfast',
+      longitude: -5.934033750117038,
+      latitude: 54.59882869880269
+    },
+    {
+      name: 'New York',
+      longitude: -74,
+      latitude: 40.721
+    },
+    {
+      name: 'London',
+      longitude: 0,
+      latitude: 50.5
+    },
+    {
+      name: 'Hong Kong',
+      longitude: 114.16,
+      latitude: 22.3
+    },
+    {
+      name: 'Singapore',
+      longitude: 103.8,
+      latitude: 1.35
+    }
+  ])
+
+  const RenderMarker = ({ name, longitude, latitude }) => {
+    console.log(longitude, latitude)
+    return (
+    <Marker
+      key={name}
+      longitude={longitude}
+      latitude={latitude}
+      pitchAlignment={'map'}
+    >
+      <div className="marker"><span></span></div>
+    </Marker>
+  )}
+
+  return (
+    <div className="map-container">
+      <ReactMapGL
+        mapboxAccessToken={mapboxAccessToken}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        style={mapStyle}
+        initialViewState={{
+          longitude: 0,
+          latitude: 45,
+          zoom: 0
+        }}
+        onClick={e => console.log(e.lngLat)}
+        
+      >
+        {markers.map(RenderMarker)}
+      </ReactMapGL>
+    </div>
+  )
+}
