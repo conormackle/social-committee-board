37d388eec37183b4b36dab70ed82da5ad1a89e52
hbrankinAQ <harry.brankin@aquaq.com>
2022-03-01 14:48:08 +0000

    Added Event categories

--- scb/src/main/java/com/aquaq/scb/entities/events/EventsModel.java
+++ scb/src/main/java/com/aquaq/scb/entities/events/EventsModel.java
@@ -1,4 +1,5 @@
 package com.aquaq.scb.entities.events;
+import com.aquaq.scb.entities.posts.PostCategoriesModel;
 import com.aquaq.scb.entities.users.UsersModel;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
@@ -33,6 +34,12 @@ public class EventsModel {
     @ManyToOne
     @JoinColumn(name = "created_by_user_id")
     private UsersModel user;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToOne
+    @JoinColumn(name = "event_category_id")
+    private EventCategoriesModel eventCategory;
 
     public Integer getId() {
         return id;
