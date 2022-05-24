d5451ed6a91d9ccee52a2dbc22df1143823d5923
hbrankinAQ <harry.brankin@aquaq.com>
2022-05-24 16:40:12 +0100

    updated ebextensions

--- scb/src/main/resources/deployment-dev/.platform/hooks/postdeploy/00_get_cert.sh
+++ scb/src/main/resources/deployment-dev/.platform/hooks/postdeploy/00_get_cert.sh
@@ -1,1 +1,2 @@
-sudo certbot --nginx -d bulletinboard-env.eba-nmzqbvuv.eu-west-1.elasticbeanstalk.com -d www.bulletinboard-env.eba-nmzqbvuv.eu-west-1.elasticbeanstalk.com
+#!/usr/bin/env bash
+sudo certbot -n -d bulletinboard-env.eba-nmzqbvuv.eu-west-1.elasticbeanstalk.com --nginx --agree-tos --email harry.brankin@aquaq.co.uk
--- scb/src/main/resources/deployment-dev/.platform/hooks/postdeploy/lf_test.sh
+++
@@ -1,1 +0,0 @@
-sudo certbot --nginx -d bulletinboard-env.eba-nmzqbvuv.eu-west-1.elasticbeanstalk.com -d www.bulletinboard-env.eba-nmzqbvuv.eu-west-1.elasticbeanstalk.com
